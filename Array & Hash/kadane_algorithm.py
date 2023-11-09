fec9e23f729d36f55b3fb7ec84365287a6d59c8c
Iqra Muzaffar <iqramuzaffar2002@gmail.com>
2023-11-09 17:38:43 +0500

    problem solve

--- Array & Hash/kadane_algorithm.py
+++ Array & Hash/kadane_algorithm.py
@@ -1,16 +1,22 @@
 # Problem link: https://leetcode.com/problems/maximum-subarray/
-# Best solution explanation: https://www.youtube.com/watch?v=5WZl3MMT0Eg
+#  solution explanation: https://www.youtube.com/watch?v=AHZpyENo7k4&t=939s
+
 
 # Solution 1:
 # Complexity: O(n)
 
 def maxSubArray(nums):
     maxSum = nums[0]
-    currentSum = 0
-    for n in nums:
-        if currentSum < 0:
-            currentSum = 0
-        currentSum += n
-        maxSum = max(maxSum, currentSum)
+    sum = 0
+    for item in nums:
+        if sum < 0:
+            sum = 0
+        sum += item
+        if sum>maxSum:
+            maxSum=sum
+        
 
     return maxSum
+
+
+
