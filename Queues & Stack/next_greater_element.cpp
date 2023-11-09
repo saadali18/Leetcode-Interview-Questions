461689a27bae44d5816a191afe5321776ef30bd8
Zummer <zummerhanif1@gmail.com>
2023-11-09 18:53:56 +0500

    Add the cpp most optimised  solution of NextgreaterElement Problem

---
+++ Queues & Stack/next_greater_element.cpp
@@ -0,0 +1,30 @@
+//Problem Link : https://leetcode.com/problems/next-greater-element-i/description/
+
+//Solution 1:
+//Complexity: O(N)
+
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+    vector <int> nge;
+    stack<int> st;   
+    unordered_map<int,int>map;
+    for (int i = 0; i < nums2.size(); i++)
+    {
+        while((! st.empty())  && (nums2[i]>nums2[st.top()])){
+            map[nums2[st.top()]] = nums2[i];
+            st.pop();
+        }
+        st.push(i);
+    }
+    while(! st.empty()){
+        map[nums2[st.top()]] = -1;
+        st.pop();
+    }
+    for(int i=0;i<nums1.size();i++){
+        nge.push_back(map[nums1[i]]);
+    }
+    return nge;   
+    }
+};
+
