712a5d100c4baec76f7180b40aa6c63107edef8a
Iqra Muzaffar <iqramuzaffar2002@gmail.com>
2023-11-09 20:13:28 +0500

    changes done

--- Array & Hash/kdane_algorithm.cpp
+++ Array & Hash/kdane_algorithm.cpp
@@ -24,3 +24,33 @@ public:
         return maxi;
     }
 };
+
+// solution 2
+//  solution link : https://youtu.be/HCL4_bOd3-4?si=WJ4Lb92yGLDT1Xev
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int maxSubArray(vector<int> &nums)
+{
+    int maxSum = nums[0];
+    int currentSum = 0;
+
+    for (int item : nums)
+    {
+        if (currentSum < 0)
+        {
+            currentSum = 0;
+        }
+        currentSum += item;
+
+        if (currentSum > maxSum)
+        {
+            maxSum = currentSum;
+        }
+    }
+
+    return maxSum;
+}
